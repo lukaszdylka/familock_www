05774963c87c33ea6813db8cfbf25d698b4349d9
Luk'ash <143443457+lukaszdylka@users.noreply.github.com>
2026-08-21 14:56:45 +0200

    Update Google Ads account ID for Familock

--- worker.js
+++ worker.js
@@ -1,3 +1,21 @@
+const LEGACY_GOOGLE_ADS_ID = 'AW-969004050';
+const GOOGLE_ADS_ID = 'AW-18392650191';
+
+const googleAdsIdShim = `<script>
+(function () {
+  var legacyAdsId = '${LEGACY_GOOGLE_ADS_ID}';
+  var currentAdsId = '${GOOGLE_ADS_ID}';
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    if (arguments.length > 1 && arguments[1] === legacyAdsId) {
+      arguments[1] = currentAdsId;
+    }
+    window.dataLayer.push(arguments);
+  };
+})();
+</script>`;
+
 export default {
   async fetch(request, env) {
     const response = await env.ASSETS.fetch(request);
@@ -8,6 +26,11 @@ export default {
     }
 
     return new HTMLRewriter()
+      .on('head', {
+        element(head) {
+          head.append(googleAdsIdShim, { html: true });
+        }
+      })
       .on('body', {
         element(body) {
           body.append('<script src="/analytics-events.js" defer></script>', { html: true });
