4b001cd9e5d23886814ad929f15ecd36ee9a4331
Luk'ash <143443457+lukaszdylka@users.noreply.github.com>
2026-08-24 07:49:04 +0200

    Dodaj informację o dopłacie za grę o 23:30

--- worker.js
+++ worker.js
@@ -16,6 +16,8 @@ const googleAdsIdShim = `<script>
 })();
 </script>`;
 
+const lateNightPriceNote = '<p class="price-note" style="margin-top:.85rem;color:var(--red-glow)">Piątek i sobota, godz. 23:30: obowiązuje dopłata 20 zł do ceny grupy.</p>';
+
 export default {
   async fetch(request, env) {
     const response = await env.ASSETS.fetch(request);
@@ -25,10 +27,20 @@ export default {
       return response;
     }
 
+    let starzikPriceBlockIndex = 0;
+
     return new HTMLRewriter()
       .on('head', {
         element(head) {
           head.append(googleAdsIdShim, { html: true });
+        }
+      })
+      .on('#tab-starzik .price-block', {
+        element(priceBlock) {
+          starzikPriceBlockIndex += 1;
+          if (starzikPriceBlockIndex === 2) {
+            priceBlock.append(lateNightPriceNote, { html: true });
+          }
         }
       })
       .on('body', {
